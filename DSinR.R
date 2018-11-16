e7ad50d4a8776a23037c4a61e0258911faf161aa
maharshithakker1234 <pg18maharshi_t@mdi.ac.in>
2018-11-16 22:02:11 +0530

    latest

--- DSinR.R
+++ DSinR.R
@@ -128,7 +128,7 @@ m2[,-4]
 colSums(m1)
 rowMeans(m1)
 
-rbind(m1,m2[,-4]) #column bind is to add next to the last col
+rbind(m1,m2[,-4]) #added below
 
 t(m1) #transpose
 m1
@@ -140,6 +140,59 @@ addmargins(m1,margin=1,sd) #row-wise
 addmargins(m1,margin=1:2,mean) #both
 
 #dataframe ----
+#create and combine vectors
+(rollno=1:30)
+sname=paste('student',1:30,sep='')
+set.seed(1234) #number can be anything, just to get equal number of MS and Fs
+(gender1=sample(c('M','F'),size=30,replace=T)) #randomly pick from M and F 30 times
+table(gender) #show how many Ms and Fs are picked
+
+(gender=sample(c('M','F'),size=30,replace=T, prob=c(0.7,0.3)))
+
+#prop.table(table(gender)) #proportion form
+
+(marks1=floor(rnorm(30,mean=50,sd=10)))
+(marks2=ceiling(rnorm(30,40,5)))
+(course=sample(c('MBA','BBA'),size=30,replace=T,prob=c(0.5,0.5)))
+
+rollno; sname; gender 
+marks1; marks2; course
+
+#create DF
+df1=data.frame(rollno,sname,gender,marks1,marks2,course,stringsAsFactors = F)
+#factors are categories
+
+str(df1) #structure of df
+
+head(df1) #by default, top 6
+head(df1,n=3)
+tail(df1)
+
+class(df1)
+
+summary(df1)
+
+df1$rollno #print only one column
+
+df1$gender=factor(df1$gender) #to convert gender into category, i.e. nominal data
+df1$course=factor(df1$course)
+
+df1[1,2]
+
+head(df1[,c(2,4)])
+
+df1[marks1>50 & gender=='F',c('sname','course')]
+
+df1[marks1>50 & gender=='F',] #comma is common sense
+
+names(df1) #names od columns
+dim(df1)
+
+aggregate(df1$marks1,by=list(df1$gender),FUN=max)
+#by gender, max marks
+
+aggregate(marks1~gender,data=df1,FUN=max)
+#same thing, another way
 
 
 
