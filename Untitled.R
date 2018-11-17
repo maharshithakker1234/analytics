0755f4e66c839ea0f3fe974cc9b2b0eb51752511
maharshithakker1234 <pg18maharshi_t@mdi.ac.in>
2018-11-17 10:41:42 +0530

    csv basics

---
+++ Untitled.R
@@ -0,0 +1,12 @@
+getwd() #pwd
+data = read.csv("SalesRecords.csv") #get data into this variable
+print(data) #same as simply writing 'data'
+
+print(is.data.frame(data))
+ncol(data)
+nrow(data)
+
+#splitting the data set into training set and test set
+
+install.packages('')
+
